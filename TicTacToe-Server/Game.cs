f7d7f4b720e18b6c2528debe0b4170054996fbdc
Maciek Klimowski <maciek.klimowski44@gmail.com>
2017-02-25 20:06:06 +0100

    Added body of missing functions

--- TicTacToe-Server/Game.cs
+++ TicTacToe-Server/Game.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,40 +16,50 @@ namespace TicTacToe_Server
         public Game()
         {
             Dashboard = new Field[3,3];
+            
+            //Array initializing
+            for (int x = 0; x < 3; x++)
+                for (int y = 0; y < 3; y++)
+                    Dashboard[x, y] = Field.Empty;
         }
 
         //Maciek
         //Przeprowadza cala gre
         //Zwraca rezultat ostatniego ruchu
-        public Result Move(int position, Player player) //przekażmy może lepiej całego playera bo by trzeba enuma kopiować
+        public Result Move(int position, Player player) //przekażmy może lepiej całego playera bo by trzeba enuma kopiować //ale wtedy nie wiem ktory player jest pierwszy
         {
-            return Result.Nothing; //TODO: temporary
+            var field = Field.Player1; //TODO: temporary
+            Dashboard[position%3, position/3] = field;
+
+            if(CheckIfPlayerWon())
+                return Result.Win;
+
+            if(CheckIfIsAnyEmptyField())
+                return Result.Nothing;
+
+            return Result.Draw;
         }
 
-        //if field equals Empty - nobody won, if field is Player1 - first player won, etc
         //Maciek
-        //zmienic na bool
-        public Field CheckIfSomeoneWon()
+        private bool CheckIfPlayerWon()
         {
-            if(CheckIfPlayerWon(Field.Player1))
-                return Field.Player1;
-
-            if(CheckIfPlayerWon(Field.Player2))
-                return Field.Player2;
-
-            return Field.Empty;
+            return Dashboard[0, 0] == Dashboard[0, 1] && Dashboard[0, 1] == Dashboard[0, 2] && Dashboard[0, 0] != Field.Empty
+                   || Dashboard[1, 0] == Dashboard[1, 1] && Dashboard[1, 1] == Dashboard[1, 2] && Dashboard[1, 0] != Field.Empty
+                   || Dashboard[2, 0] == Dashboard[2, 1] && Dashboard[2, 1] == Dashboard[2, 2] && Dashboard[2, 0] != Field.Empty
+                   || Dashboard[0, 0] == Dashboard[1, 0] && Dashboard[1, 0] == Dashboard[2, 0] && Dashboard[0, 0] != Field.Empty
+                   || Dashboard[0, 1] == Dashboard[1, 1] && Dashboard[1, 1] == Dashboard[2, 1] && Dashboard[0, 1] != Field.Empty
+                   || Dashboard[0, 2] == Dashboard[0, 1] && Dashboard[1, 2] == Dashboard[2, 2] && Dashboard[0, 2] != Field.Empty
+                   || Dashboard[0, 0] == Dashboard[1, 1] && Dashboard[1, 1] == Dashboard[2, 2] && Dashboard[0, 0] != Field.Empty
+                   || Dashboard[2, 0] == Dashboard[1, 1] && Dashboard[1, 1] == Dashboard[0, 2] && Dashboard[2, 0] != Field.Empty;
         }
 
-        private bool CheckIfPlayerWon(Field player)
+        private bool CheckIfIsAnyEmptyField()
         {
-            return Dashboard[0, 0] == player && Dashboard[0, 1] == player && Dashboard[0, 2] == player
-                   || Dashboard[1, 0] == player && Dashboard[1, 1] == player && Dashboard[1, 2] == player
-                   || Dashboard[2, 0] == player && Dashboard[2, 1] == player && Dashboard[2, 2] == player
-                   || Dashboard[0, 0] == player && Dashboard[1, 0] == player && Dashboard[2, 0] == player
-                   || Dashboard[0, 1] == player && Dashboard[1, 1] == player && Dashboard[2, 1] == player
-                   || Dashboard[0, 2] == player && Dashboard[1, 2] == player && Dashboard[2, 2] == player
-                   || Dashboard[0, 0] == player && Dashboard[1, 1] == player && Dashboard[2, 2] == player
-                   || Dashboard[2, 0] == player && Dashboard[1, 1] == player && Dashboard[0, 2] == player;
+            for (int x = 0; x < 3; x++)
+                for (int y = 0; y < 3; y++)
+                    if (Dashboard[x, y] == Field.Empty)
+                        return true;
+            return false;
         }
     }
 
